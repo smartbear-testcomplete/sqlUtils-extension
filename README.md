3f94dbdcfad352a4faa9af9ca35feccbd69b081a
Robert MArtin <38894595+tristaanogre@users.noreply.github.com>
2018-05-01 18:18:07 -0400

    Initial setup
    Setting up initial respostory for SQLUtilities

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# sqlUtils-extension
+# README #
+
+This extensions is intended to make it easier and portable to do SQL queries and operations against an SQL database without having to re-write code all over the place.  An ADODB Connection object is created in this extension and several methods are provided both to configure the environment as well as to execute SQL queries and commands on a configured SQL database.
+
+## ABSTRACT ##
+
+### Properties ###
+
+* DatabaseName - Read/Write - Stores the database name for the SQL database for all queries run within the extension. Value is persisted within a TestComplete/TestExecute session but is not persisted across sessions.
+* SQLServerName - Read/Write - Stores the SQL Server name for the SQL database for all queries run within the extension. Value is persisted within a TestComplete/TestExecute session but is not persisted across sessions.
+* SQLUserName - Read/Write - Stores the SQL UserName for the SQL database for queries run within the extension. This field is only necessary if using either MYSQL or if using MSSQL with PROMPT database security. Value is persisted within a TestComplete/TestExecute session but is not persisted across sessions.
+* SQLPassWord - Read/Write - Stores the SQL Password for the SQL database for queries run within the extension. This field is only necessary if using either MYSQL or if using MSSQL with PROMPT database security. Value is persisted within a TestComplete/TestExecute session but is not persisted across sessions.
+* ConnectionString - Read Only - A connection string constructed and initialized based upon the properties entered and on the values of the SQLType and SQLSecurity
+
+### Methods ###
+
+The first two methods below are used to set options to persist. They can be called once and only once and never again if so desired just for initial set up.
+
+* SetSQLType - no value returned - This sets the SQL connection type for the TestComplete environment. When this method is called and a value is passed, if the value is valid, that SQL connection type is persisted across TestComplete sessions on the same workstation. This allows the automation engineer to set up their environment for a particular SQL type and not have to reconfigure it each time.  Valid options are: MSSQL and MYSQL_351._
+* SetSQLSecurityType - no value returned - This applies only to MSSQL connection type. When this method is called and a value is passed, if the value is valid, that SQL security type is persisted across TestComplete sessions on the same workstation. This allows the automation engineer to set up their environment for a particular SQL security configuration and not have to reconfigure it each time. This configures either integrated security or security that embeds a username and password. Valud values are: INTEGRATED and PROMPT
+
+Two additional methods are provided for instantiating objects to be used for executing SQL queries.
+
+* **NewQueryObject** - Returns an object with two properties based upon the parameters passed in. The first parameter is a string that contains the actual SQL query text itself. The second parameter is an JScript object containing any parameters needed to execute the SQL query (see example below)
+* **NewSQLQuery** - Returns an object for executing an SQL query. It has one parameters, that being a query object as created by NewQueryObject.
+
+The following method is provided to convert a date value into something meanful for SQL
+
+* **FormateDateForSQL** - returns a date in the format of YYYY-MM-DD - parameter is an integer value - Use this method to add or subtract days from the current date (Today) and format the result into a date string that matches SQL formatting
+
+# Example #
+
+The following example demonstrates using this extension in a project:
+
+```
+#!javascript
+//The following function can be included in any project but it only ever needs to be run the first time to set up the desired settings
+function SQLOptions(){
+    SQLUtilities.SetSQLType('MSSQL'); //Can be either MSSQL or MYSQL_351
+	SQLUtilities.SetSQLSecurityType('INTEGRATED'); //Can be either INTEGRATED or PROMPT
+}
+
+//Now, if I want to run an SQL query against my database where I log the street address for all addresses in Pennsylvania, USA
+function getPAStreetAddresses() {
+    var mySQLQuery;
+    var queryObject;
+	var myRecords;
+    SQLUtilities.DatabaseName = 'MyDatabase';
+	SQLUtilities.SQLServerName = 'MyServer';
+	SQLUtilities.SQLUserName = 'myusername'; //This is not necessary since I'm using integrated security. Provided in example to allow for editing and adaptation
+	SQLUtilities.SQLPassWord = 'mypassword'; //This is not necessary since I'm using integrated security. Provided in example to allow for editing and adaptation
+    queryObject = SQLUtilities.NewQueryObject('SELECT * FROM ADDRESSES WHERE STATE = :STATE and COUNTRY = :COUNTRY', {STATE: 'PA', COUNTRY: 'USA'})
+	mySQLQuery = SQLUtilities.NewSQLQuery(queryObject);
+	mySQLQuery.open();
+	myRecords = mySQLQuery.execute();
+	while (!myRecords.EOF) {
+	    Log.Message('Street Address: ' + myRecords.Fields.Item('STREETADDRESS').Value);
+		myRecords.MoveNext();
+	}
+	mySQLQuery.closeAndNull;
+}
+```
+
---
+++ Source/SQLUtilities.js
@@ -0,0 +1,242 @@
+﻿//This code and the associated script extension are provided under a Creative Commons Attribution-ShareAlike 4.0 International License.
+//You may use and modify this code according to the license, giving appropriate attribution and, if you distribute it, you must do so
+//under the same license.
+
+var databaseName;
+var SQLServerName;
+var connectionString;
+var userName;
+var passWord;
+
+
+function getDatabaseName() {
+    return databaseName;
+}
+
+function setDatabaseName(value) {
+    databaseName = value;
+}
+
+function getSQLServerName() {
+    return SQLServerName;
+}
+
+function setSQLServerName(value) {
+    SQLServerName = value;
+}
+
+function getConnectionString() {
+    connectionString = buildConnectionString();
+    return connectionString;
+}
+
+function getUserName() {
+    return userName;
+}
+
+function setUserName(value) {
+    userName = value;
+}
+
+function getPassWord() {
+    return passWord;
+} 
+
+function setPassWord(value) {
+    passWord = value;
+}
+
+function setSQLType(value){
+    Options.SQLType = aqString.ToUpper(value);    
+}
+
+function setSecurityType(value){
+    if (Options.SQLType == 'MSSQL') {
+        Options.SecurityType = aqString.ToUpper(value);
+    }
+    else {
+        Options.SecurityType = 'PROMPT';
+    }   
+}
+
+function buildConnectionString() {
+    var localString;
+    switch (Options.SQLType) {
+        case 'MSSQL': 
+            localString = 'Provider=SQLOLEDB.1;Persist Security Info=False;Initial Catalog=';
+            localString = localString + databaseName;
+            localString = localString + ';Data Source=' + SQLServerName;
+            if (Options.SecurityType == 'PROMPT') {
+                localString = localString + ';User ID=' + userName + ';Password=' + passWord + ';';
+            }
+            else {
+                localString = localString + ';Integrated Security=SSPI';
+            }
+            break;
+        case 'MYSQL_351':
+            localString = 'Driver={MySQL ODBC 3.51 Driver};Server=' + SQLServerName + ';Database=' + databaseName + ';User=' + userName + ';Password=' + passWord + ';Option=3';
+            break;
+        default :
+            Log.Warning('Unknown SQL type encountered. Defaulting to MSSQL with Integrated Security');
+            Options.SQLType = 'MSSQL';
+            Options.SecurityType = 'INTEGRATED';
+            localString = 'Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=';
+            localString = localString + databaseName;
+            localString = localString + ';Data Source=' + SQLServerName;          
+    }
+
+    return localString;
+}
+
+function queryObject(query, parameters) {
+    this.string = query;
+    if (parameters == undefined) {
+        parameters = [];
+    }
+    this.parameterArray = [];
+    for (parameter in parameters) {
+        this.parameterArray.push({name: param, value: parameters[param]});
+    }
+}
+
+
+function getSQLFromFile(filePath) {
+    return aqFile.ReadWholeTextFile(filePath, aqFile.ctANSI);
+}
+
+function open() {
+//Opens the connection on the query object and sets it as the Active Connection on the command object property
+    this.localConnection.Open();
+    this.localCommand.CommandObject.ActiveConnection = this.localConnection;    
+}
+
+function closeAndNull() {
+//Closes the connection on the object and sets the connection and command objects to null to free up memory space
+    if (this.localConnection.State != 0) {
+        this.localConnection.Close();
+    }
+    this.localConnection = null;
+    this.localCommand = null;
+}
+
+function execute() {
+//assigns the query to the command text, parses it for parameters, and then executes the query as a command, returning any recordset
+    this.localCommand.CommandText = this.queryObject.queryString;
+    this.localCommand.Parameters.ParseSQL(this.localCommand.CommandText, true);        
+    for (var i = 0; i < this.queryObject.parameterArray.length; i++) {
+
+        this.localCommand.Parameters.ParamByName(this.queryObject.parameterArray[i].name).Value = this.queryObject.parameterArray[i].value;
+    }
+    return this.localCommand.Execute();
+}
+
+function sqlQuery(sqlString, parameters, stringIsFile) {
+    try {
+        if (stringIsFile == undefined) {
+            stringIsFile = false;
+        }
+        var localObject = {};
+        var queryString;
+        stringIsFile ? queryString = getSQLFromFile(sqlString) : queryString = sqlString;
+        localObject.queryObject = new queryObject(sqlString, parameters);
+        localObject.localConnection = ADO.CreateConnection();
+        localObject.localConnection.ConnectionString = getConnectionString();
+        localObject.localCommand = ADO.CreateADOCommand();
+        localObject.open = open;
+        localObject.execute = execute;
+        localObject.closeAndNull = closeAndNull;
+        return localObject;
+    }
+    catch (exception) {    
+        Log.Error('Could not execute sqlQuery: ' + exception.message, exception.stack);
+    }
+}
+
+//function ExecSQLQueryFile(FileName)
+//{
+//    try
+//    {
+//        var RowCount;    
+//        connectionObject.ConnectionString = getConnectionString();
+//        var SQLQuery = aqFile.ReadWholeTextFile(aqConvert.VarToStr(FileName), aqFile.ctANSI);
+//        connectionObject.Open();
+//        connectionObject.Execute(SQLQuery, RowCount);
+//        connectionObject.Close();
+//        return RowCount;
+//    }
+//    catch (e)
+//    {
+//        Log.Error("Error executing Query file " + aqConvert.VarToStr(FileName) + ": " + e.description);
+//        return -1;
+//    }
+//}
+//
+//function ExecSQLQueryFromString(QueryString)
+//{
+//    try
+//    {
+//        var RowCount;    
+//        connectionObject.ConnectionString = getConnectionString();
+//        connectionObject.Open();        
+//        connectionObject.Execute(aqConvert.VarToStr(QueryString), RowCount);
+//        connectionObject.Close();
+//        return RowCount;        
+//    }    
+//    catch (e)
+//    {
+//        Log.Error("Error executing QueryString.  See Additional Information for QueryString value: " + e.description, aqConvert.VarToStr(QueryString));
+//        return -1;
+//    }
+//}
+//
+//function ReturnQueryRecordSetFromString(QueryString)
+//{
+//    try
+//    {
+//        connectionObject.ConnectionString = getConnectionString();
+//        connectionObject.Open();        
+//        var RecordSet = connectionObject.Execute(aqConvert.VarToStr(QueryString));
+//        return RecordSet;
+//    }
+//    catch (e)
+//    {
+//        Log.Error("Error running QueryString.  See Additional Information for QueryString value: " + e.description, aqConvert.VarToStr(QueryString))
+//        return aqObject.EmptyVariant;
+//    }    
+//}    
+//
+//function ExecuteStoredProcedure(ProcedureName)
+//{
+//    try
+//    {
+//        var RowCount;
+//        connectionObject.ConnectionString = getConnectionString();
+//        connectionObject.Open();        
+//        connectionObject.Execute("EXEC " + aqConvert.VarToStr(ProcedureName), RowCount);
+//        connectionObject.Close();
+//        return RowCount;
+//    }
+//    catch (e)
+//    {
+//        Log.Error("Error running Stored procedure " + aqConvert.VarToStr(ProcedureName) + ": " + e.description);
+//        return -1;
+//    }
+//}
+
+
+function FormatDateForSQL(OffsetDays)
+{
+    try
+        {
+        OffsetDays = aqConvert.VarToInt(OffsetDays);
+        }
+    catch (e)
+        {
+        Log.Warning("Value of OffsetDays is non-numeric, using zero (0) instead");
+        OffsetDays = 0;                                                    
+        }
+    
+    var DateValue = aqDateTime.Today();
+    DateValue = aqDateTime.AddDays(DateValue, OffsetDays);
+    return aqConvert.DateTimetoFormatStr(DateValue, '%Y-%m-%d');
+}
